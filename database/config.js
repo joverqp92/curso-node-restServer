5e08d17db62771b1e9d408f97a32a69d12752484
Jover Quezada <joverquezadap@gmail.com>
2023-09-18 19:14:43 -0500

    restserver actualizado v3

--- database/config.js
+++ database/config.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-
+        console.log(process.env.MONGODB_ATLAS)
         await mongoose.connect(process.env.MONGODB_ATLAS,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
