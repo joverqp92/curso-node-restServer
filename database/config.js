76245b9eebd8fb5d5dabf2b3d1ebd406263ae7f9
Jover Quezada <joverquezadap@gmail.com>
2023-09-18 19:41:43 -0500

    Using CNN instead of atlas

--- database/config.js
+++ database/config.js
@@ -5,8 +5,8 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-        console.log(process.env.MONGODB_ATLAS)
-        await mongoose.connect(process.env.MONGODB_ATLAS,{
+        console.log(process.env.MONGODB_CNN)
+        await mongoose.connect(process.env.MONGODB_CNN,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
